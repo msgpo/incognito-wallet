264d7e091e7b5d8003e603121b10258aec206140
0xSiris <siris@incognito.org>
2020-04-14 14:12:00 +0700

    test 2

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 
-echo "
-PASSWORD_SECRET_KEY=$PASSWORD_SECRET_KEY
-PASSPHRASE_WALLET_DEFAULT=$PASSPHRASE_WALLET_DEFAULT
-API_MINER_URL=$API_MINER_URL
-PASS_HOSPOT=$PASS_HOSPOT
-" > .env
-
-if [[ ! -z "${IOS_BUILD_CONFIGURATION}" ]]; then
-  FILE=$(find ~/*/work/1/a/build -type f -name "Incognito.ipa" -print -quit)
-  LAST_COMMIT_MESSAGE="$(git show -s --format=%s)"
-  APP_VERSION="$(sed 's/.*\"version\": \"\(.*\)\".*/\1/;t;d' ./package.json)"
-  NEW_FILE_NAME=$APP_VERSION-IOS_BUILD_CONFIGURATION
-
-  cp $FILE $NEW_FILE_NAME.ipa
-
-  chmod +x .circleci/slack-upload.sh
-  .circleci/slack-upload.sh -f $NEW_FILE_NAME.ipa -c '$SLACK_CHANNEL' -s $SLACK_API_KEY -n "$NEW_FILE_NAME" -x "\"$LAST_COMMIT_MESSAGE\""
-fi
+#if [[ ! -z "${IOS_BUILD_CONFIGURATION}" ]]; then
+#  FILE=$(find ~/*/work/1/a/build -type f -name "Incognito.ipa" -print -quit)
+#  LAST_COMMIT_MESSAGE="$(git show -s --format=%s)"
+#  APP_VERSION="$(sed 's/.*\"version\": \"\(.*\)\".*/\1/;t;d' ./package.json)"
+#  NEW_FILE_NAME=$APP_VERSION-IOS_BUILD_CONFIGURATION
+#
+#  cp $FILE $NEW_FILE_NAME.ipa
+#
+#  chmod +x .circleci/slack-upload.sh
+#  .circleci/slack-upload.sh -f $NEW_FILE_NAME.ipa -c '$SLACK_CHANNEL' -s $SLACK_API_KEY -n "$NEW_FILE_NAME" -x "\"$LAST_COMMIT_MESSAGE\""
+#fi
